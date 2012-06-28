e3ef77723e1280d83234171e51c8e56449be7b05
Shad <shad.downey@gmail.com>
2012-06-28 16:45:02 -0700

    Clearer how Request and Response can be extended in App

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,7 +1,7 @@
 <?php
 define('PaZsCA8p','Yeah!');
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'bootstrap.php');
-$Response = Controller::handleRequest(new Request($_SERVER, $_GET, $_POST, $_COOKIE));
+$Response = Controller::handleRequest(new AppRequest($_SERVER, $_GET, $_POST, $_COOKIE));
 if(!headers_sent() && OUTPUT_BUFFER){ob_start("ob_gzhandler");}
 $Response->render();
 if(!headers_sent() && OUTPUT_BUFFER){ob_end_flush();}
