7cf8f3505854bb0426502d73dcb0029b3ec4d127
Shad <shad.downey@gmail.com>
2012-09-03 20:03:18 -0700

    reworked Response

--- app/classes/App/Response.php
+++ app/classes/App/Response.php
@@ -1,16 +1,11 @@
 <?php
 
-class AppResponse extends Response {
+class AppResponse extends Response
+{
 	protected $appVars = array(
-		'errors' => array(),
 		'base_url' => BASE_URL,
+		'debug' => DEBUG,
 	);
-	
-	protected function load() {
-		parent::load();
-		$this->set('debug', DEBUG);
-	}
-	
+
 }
 
-?>
