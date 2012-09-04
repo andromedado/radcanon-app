9a87b5d08d7ab9115e65b16f9f8493be332c855d
Shad <shad.downey@gmail.com>
2012-09-03 20:03:48 -0700

    reworked constants

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,8 +1,15 @@
 <?php
 define('PERMIT_DEBUG', true);
-define('DEFAULT_SITE_HOST', 'radcanon.com');
+define('DEFAULT_SITE_HOST', 'radcanon-app.com');
+if (empty($_SERVER['HTTP_HOST'])) $_SERVER['HTTP_HOST'] = DEFAULT_SITE_HOST;
+define('SITE_HOST', $_SERVER['HTTP_HOST']);
+define('DEFAULT_PAGE_TITLE', 'RadCanon - App');
+define('CURRENT_PROTOCOL', 'http' . (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443 ? 's' : '') . '://');
+define('BASE_URL', CURRENT_PROTOCOL . SITE_HOST . '/');
 define('MAIL_FROM', 'Info <info@' . DEFAULT_SITE_HOST . '>');
 define('APP_SUB_DIR' , '');//No Trailing Slash
+define('APP_NAME', 'RADCanon App');
+define('USE_HTACCESS', true);
 
 define('DS', DIRECTORY_SEPARATOR);
 define('APP_DIR', dirname(__DIR__) . DS);
@@ -12,11 +19,7 @@ define('RADCANON_DIR', LIB_DIR . 'radcanon' . DS);
 define('DEBUG', PERMIT_DEBUG && isset($_SERVER['REMOTE_ADDR']) && in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1')));
 define('OUTPUT_BUFFER', !DEBUG);
 define('CONFIG_DIR', __DIR__ . DS);//With Trailing Slash
-if (empty($_SERVER['HTTP_HOST'])) $_SERVER['HTTP_HOST'] = DEFAULT_SITE_HOST;
-define('SITE_HOST', $_SERVER['HTTP_HOST']);
 if (empty($_SERVER['REQUEST_URI'])) $_SERVER['REQUEST_URI'] = '/';
-define('CURRENT_PROTOCOL', 'http' . (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443 ? 's' : '') . '://');
-define('BASE_URL', CURRENT_PROTOCOL . SITE_HOST);
 define('APP_CLASS_DIR', APP_DIR . 'classes' . DS);
 define('CACHE_DIR', APP_DIR . 'cache' . DS);
 define('TEMPLATE_CACHE_DIR', CACHE_DIR . 'templates');
