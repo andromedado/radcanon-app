8fb6dc361d46df25f362b14e72c025508050d669
Shad <shad.downey@gmail.com>
2012-09-26 10:03:00 -0700

    cleanup index, handles exceptions now!

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,8 +1,22 @@
 <?php
-define('PaZsCA8p','Yeah!');
-require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'bootstrap.php');
-$Response = Controller::handleRequest(new AppRequest($_SERVER, $_GET, $_POST, $_COOKIE));
-if(!headers_sent() && OUTPUT_BUFFER){ob_start("ob_gzhandler");}
-$Response->render();
-if(!headers_sent() && OUTPUT_BUFFER){ob_end_flush();}
+require_once(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'bootstrap.php');
+try {
+	$Response = Controller::handleRequest(new AppRequest($_SERVER, $_GET, $_POST, $_COOKIE));
+	if(!headers_sent() && OUTPUT_BUFFER){ob_start("ob_gzhandler");}
+	$Response->render();
+	if(!headers_sent() && OUTPUT_BUFFER){ob_end_flush();}
+} catch (Exception $e) {
+	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+?><!DOCTYPE html>
+<html>
+	<head>
+		<title>500 Internal Server Error</title>
+	</head>
+	<body>
+		<h1>500 Internal Server Error</h1>
+		<h2><?php echo $e->getMessage(); ?></h2>
+	</body>
+</html><?php
+}
 exit;
+
